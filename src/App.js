1843dbb71233f6e3bc6ef88a3a3c8246b7ab7a9e
adebayo mohammed <adebayomuhammad73@gmail.com>
2021-04-03 21:55:33 +0100

    chore: configuring eslint/ prettier

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
+import { Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/" exact component={} />
+        <Route path="/play" exact component={} />
+      </Switch>
     </div>
   );
 }
